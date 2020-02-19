8d4a930044847093e74c0e425742b9ff6fcc71a4
fbaileyjr <frankbaileyjr@gmail.com>
2020-02-18 23:55:15 -0500

    Final push of changes and output files

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,16 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=spiralmemory/udacity-project-4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+export DOCKER_ID_USER="spiralmemory"
+docker login
+docker tag udacity-project-4 $DOCKER_ID_USER/udacity-project-4
+docker push $DOCKER_ID_USER/udacity-project-4
 
 # Step 3:
 # Push image to a docker repository
+docker push spiralmemory/udacity-project-4
